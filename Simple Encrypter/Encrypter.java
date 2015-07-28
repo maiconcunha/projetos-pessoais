548f11190b9c04e31e0af879299da6684fb09013
maiconcunha <maiconrosadacunha@gmail.com>
2015-07-28 09:22:15 -0300

    classe dos métodos de crptografia e descriptografia

---
+++ Simple Encrypter/Encrypter.java
@@ -0,0 +1,67 @@
+﻿/*
+* Este programa captura do usuário 4 inteiros e criptografa-os ou descriptografa-os.
+*
+* Para criptografar, o programa lê os digitos, determina se são 4 digitos, adiciona 7 ao valor de cada digito,
+* divide por dez e o resto dessa divisão é atribuído ao digito.
+*
+* Para descriptografar, é feito o inverso acima.
+*/
+
+public class Encrypter
+{
+    private static int firstNum, secondNum, thirdNum, fourthNum, aux;
+    
+    public static void breakDigits(int num)
+    {
+        firstNum = num % 10;
+        num /= 10;
+        secondNum = num % 10;
+        num /= 10;
+        thirdNum = num % 10;
+        num /= 10;
+        fourthNum = num % 10;
+    }
+    
+    public int encrypt(int numToEncrypt)
+    {
+        breakDigits(numToEncrypt);
+        
+        firstNum += 7;
+        secondNum += 7;
+        thirdNum += 7;
+        fourthNum += 7;
+        
+        firstNum %= 10;
+        secondNum %= 10;
+        thirdNum %= 10;
+        fourthNum %= 10;
+        
+        aux = firstNum;
+        firstNum = thirdNum;
+        thirdNum = aux;
+        aux = secondNum;
+        secondNum = fourthNum;
+        fourthNum = aux;
+        
+        return (firstNum * 1000) + (secondNum * 100) + (thirdNum * 10) + fourthNum;
+    }
+    
+    public int decrypt(int numToDecrypt)
+    {
+        breakDigits(numToDecrypt);
+        
+        aux = fourthNum;
+        fourthNum = secondNum;
+        secondNum = aux;
+        aux = thirdNum;
+        thirdNum = firstNum;
+        firstNum = aux;
+        
+        firstNum = (firstNum + 3) % 10;
+        secondNum = (secondNum + 3) % 10;
+        thirdNum = (thirdNum + 3) % 10;
+        fourthNum = (fourthNum + 3) % 10;
+        
+        return (firstNum * 1000) + (secondNum * 100) + (thirdNum * 10) + fourthNum;
+    }
+}
---
+++ Simple Encrypter/Encrypter_CLI.java
@@ -0,0 +1,63 @@
+﻿/*
+* Este programa captura do usuário 4 inteiros e criptografa-os ou descriptografa-os.
+*
+* Esta classe define a interface ao usuário, perguntando-lhe o que deseja fazer e procedendo conforme.
+*/
+
+import java.util.Scanner;
+
+public class Encrypter_CLI
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        int opt;
+
+	do {
+            System.out.printf("1 - Criptografar;\n2 - Descriptografar;\n3 - Sair;\nOpção: ");
+                opt = input.nextInt();
+
+            if (opt < 1 || opt > 3)
+                System.out.println("Atenção: opção inválida!");
+            else if (opt == 1)
+                encrypt();
+            else if (opt == 2)
+                decrypt();
+        } while (opt != 3);
+    }
+
+    public static void encrypt()
+    {
+	Scanner input = new Scanner(System.in);
+	Encrypter enc = new Encrypter();
+
+	int numToEncrypt;
+
+        System.out.print("Digite o número a ser criptografado: ");
+            numToEncrypt = input.nextInt();
+
+        if (numToEncrypt < 1000 || numToEncrypt > 9999) {
+            System.out.println("Atenção, digite apenas quatro números inteiros!");
+            encrypt();
+        } else
+            System.out.printf("\nCriptografando, aguarde...\nNúmero criptografado: %d\n\n", enc.encrypt(numToEncrypt));
+    }
+    
+    public static void decrypt()
+    {
+        Scanner input = new Scanner(System.in);
+	Encrypter enc = new Encrypter();
+        
+        int numToDecrypt;
+
+        System.out.print("Digite o número a ser descriptografado: ");
+            numToDecrypt = input.nextInt();
+
+        if (numToDecrypt < 1000 || numToDecrypt > 9999) {
+            System.out.println("Atenção, digite apenas quatro números inteiros!");
+            decrypt();
+        } else
+            System.out.printf("\nDescriptografando, aguarde...\nNúmero descriptografado: %d\n\n", enc.decrypt(numToDecrypt));
+    }
+}
